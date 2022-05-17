f7ba2d71f0027f88937ee2d0824133d1106937a8
Ankit Shrestha <ankit.shrestha196@gmail.com>
2022-05-17 09:47:13 +0545

    Initiial Commit

---
+++ math_eq.js
@@ -0,0 +1,72 @@
+// 2 + 4 * 3 - 7
+// 2 + 12 - 7
+
+
+const operators = {
+    mul: '*',
+    div: "/",
+    add: "+",
+    sub: "-"
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function sub(a, b) {
+    return a - b
+}
+
+function mul(a, b) {
+    return a * b
+}
+
+function div(a, b) {
+    return a / b
+}
+
+function calculate(a, b, operation) {
+    return operation(a, b)
+}
+
+export function parse(equation) {
+    const step = getNextStep(equation) 
+    const result = solve(step) 
+    const newEquation = replaceNextStep(equation, step, result) 
+    // return parse(newEquation)
+}
+
+function getNextStep(equation) {
+    equation = equation.split(" ")
+    for (let i = 0; i < equation.length; i++) {
+        if(equation[i] === operators.mul || equation[i] === operators.div) {
+            return [equation[i-1], equation[i], equation[i+1]]
+        }
+    }
+    for (let i = 0; i < equation.length; i++) {
+        if(equation[i] === operators.add || equation[i] === operators.sub) {
+            return [equation[i-1], equation[i], equation[i+1]]
+        }
+    }
+}
+
+function solve(step) {
+    let [fOperand, operation, sOperand] = step
+    fOperand = parseFloat(fOperand)
+    sOperand = parseFloat(sOperand)
+    switch (operation) {
+        case '*':
+            return calculate(fOperand, sOperand, mul)
+        case '/':
+            return calculate(fOperand, sOperand, div)
+        case '+':
+            return calculate(fOperand, sOperand, add)
+        case '-':
+            return calculate(fOperand, sOperand, sub)
+    }
+
+}
+
+function newEquation(equation, step, result) {
+    
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+import { parse } from "./math_eq.js"
+
+const inputEq = document.querySelector(".equation")
+const answerDiv = document.querySelector(".solution")
+
+document.addEventListener("click", e => {
+    if (!e.target.matches(".solve-btn")) return
+    const input = inputEq.value
+    if (input === "") return
+    const result = parse(input)
+    answerDiv.innerText = result
+    inputEq.innerText = ""
+})
